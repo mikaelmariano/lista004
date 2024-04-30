b40b52b54ceca4daeecad6e68c0fdfdfe762a9e3
Eudes Pereira <eudes.pereira22@gmail.com>
2024-04-29 21:05:26 -0300

    Correção exercicio 1

--- cenario1/src/br/edu/up/Programa.java
+++ cenario1/src/br/edu/up/Programa.java
@@ -3,7 +3,7 @@ package br.edu.up;
 import br.edu.up.modelos.Livro;
 
 
-public class Biblioteca {
+public class Programa {
 
     public static void main(String[] args) {
         Livro livro1 = new Livro("1598FHK", "Core Java 2", new String[]{"Cay S. Horstmann", "Gary Cornell"}, "0130819336", 2005);
